1a7ba06cebe1834ced0b40a27d818e412d6011b5
arest <arest09@mail.ru>
2023-05-22 21:13:38 +1000

    запись подходов в базу данных

--- back/app/middleware/auth.middleware.js
+++ back/app/middleware/auth.middleware.js
@@ -8,6 +8,8 @@ export const protect = async (req, res, next) => {
       const token = req.headers.authorization.split(' ')[1]
       const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
 
+      console.log(decodedToken)
+
       const user = await prisma.user.findUnique({
         where: {
           id: decodedToken.id
